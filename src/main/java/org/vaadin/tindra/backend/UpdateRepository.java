e23be92dce24a0872623188baa22a4ee95d5cae0
Matti Tahvonen <matti@vaadin.com>
2014-06-23 00:04:40 +0300

    work in progress

--- src/main/java/org/vaadin/tindra/backend/UpdateRepository.java
+++ src/main/java/org/vaadin/tindra/backend/UpdateRepository.java
@@ -1,13 +1,17 @@
 
 package org.vaadin.tindra.backend;
 
-import org.vaadin.tindra.domain.Update;
+import java.util.Date;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.vaadin.tindra.domain.Update;
 
 /**
  *
  * @author mattitahvonenitmill
  */
 public interface UpdateRepository extends JpaRepository<Update, Long> {
+    
+    public List<Update> findByTimestampGreaterThanOrderByTimestampDesc(Date start);
 
 }
--- src/main/java/org/vaadin/tindra/tcpserver/ServerThread.java
+++ src/main/java/org/vaadin/tindra/tcpserver/ServerThread.java
@@ -3,6 +3,8 @@ package org.vaadin.tindra.tcpserver;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.vaadin.tindra.domain.Update;
 
 /**
@@ -29,9 +31,14 @@ public class ServerThread extends Thread {
             // Initiate conversation with client
             GPSProtocol kkp = new GPSProtocol();
 
-            while ((inputLine = in.readLine()) != null && server.isRunning()) {
-                update = kkp.processInput(inputLine);
-                server.persist(update);
+            while ((inputLine = in.readLine()) != null && !inputLine.trim().isEmpty() && server.isRunning()) {
+                try {
+                    update = kkp.processInput(inputLine);
+                    server.persist(update);
+                } catch(Exception e) {
+                    Logger.getLogger(getClass().getName()).severe("Failed to parse message:" + inputLine);
+                    Logger.getLogger(getClass().getName()).log(Level.SEVERE,"Failed to parse message", e);
+                }
             }
 
         } catch (Exception e) {
