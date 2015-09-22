2846e09775dc92aff0b69989b783be8fa8f05d45
Matti Tahvonen <matti@vaadin.com>
2015-09-22 23:49:07 +0300

    Using Viritin CDN version during development

--- src/main/java/org/vaadin/tindra/Application.java
+++ src/main/java/org/vaadin/tindra/Application.java
@@ -1,17 +1,10 @@
 package org.vaadin.tindra;
 
 import com.vaadin.server.VaadinServlet;
-import org.apache.catalina.connector.Connector;
-import org.apache.coyote.http11.AbstractHttp11Protocol;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
-import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
-import org.springframework.boot.context.embedded.tomcat.TomcatConnectorCustomizer;
-import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.http.MediaType;
 
 @SpringBootApplication
 @EnableJpaRepositories
@@ -21,42 +14,17 @@ public class Application {
         System.setProperty("org.geotools.referencing.forceXY", "true");
         SpringApplication.run(Application.class, args);
     }
-    
+
     @Bean
     public VaadinServlet vaadinServlet() {
         return new SpringAwareTouchKitServlet();
     }
 
-    /**
-     * Configure embedded tomcat so that is use gzip for various resources.
-     * 
-     * @return 
-     */
+    // Stuff needed for widgetset CDN, not needed if using std widgetset.
+    // Run priming build (mvn install) if (generated) resource is missing.
     @Bean
-    public EmbeddedServletContainerCustomizer servletContainerCustomizer() {
-        return new EmbeddedServletContainerCustomizer() {
-            @Override
-            public void customize(
-                    ConfigurableEmbeddedServletContainer servletContainer) {
-                ((TomcatEmbeddedServletContainerFactory) servletContainer).
-                        addConnectorCustomizers(
-                                new TomcatConnectorCustomizer() {
-                                    @Override
-                                    public void customize(Connector connector) {
-                                        AbstractHttp11Protocol httpProtocol = (AbstractHttp11Protocol) connector.
-                                        getProtocolHandler();
-                                        httpProtocol.setCompression("on");
-                                        httpProtocol.setCompressionMinSize(256);
-                                        String mimeTypes = httpProtocol.
-                                        getCompressableMimeTypes();
-                                        String mimeTypesWithJson = mimeTypes + "," + MediaType.APPLICATION_JSON_VALUE + ",application/javascript";
-                                        httpProtocol.setCompressableMimeTypes(
-                                                mimeTypesWithJson);
-                                    }
-                                }
-                        );
-            }
-        };
+    public in.virit.WidgetSet viritinCdnInitializer() {
+        return new in.virit.WidgetSet();
     }
 
 }
