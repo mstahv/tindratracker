7871c5044c18c74bc5787f2883f8736322843254
Matti Tahvonen <matti@vaadin.com>
2017-05-26 10:04:33 +0300

    Updated some versions, closes  #1

--- src/main/java/org/vaadin/tindra/Application.java
+++ src/main/java/org/vaadin/tindra/Application.java
@@ -37,36 +37,4 @@ public class Application {
         return new SpringAwareTouchKitServlet();
     }
 
-    /**
-     * Configure embedded tomcat so that is use gzip for various resources.
-     *
-     * @return
-     */
-    @Bean
-    public EmbeddedServletContainerCustomizer servletContainerCustomizer() {
-        return new EmbeddedServletContainerCustomizer() {
-            @Override
-            public void customize(
-                    ConfigurableEmbeddedServletContainer servletContainer) {
-                ((TomcatEmbeddedServletContainerFactory) servletContainer).
-                        addConnectorCustomizers(
-                                new TomcatConnectorCustomizer() {
-                            @Override
-                            public void customize(Connector connector) {
-                                AbstractHttp11Protocol httpProtocol = (AbstractHttp11Protocol) connector.
-                                        getProtocolHandler();
-                                httpProtocol.setCompression("on");
-                                httpProtocol.setCompressionMinSize(256);
-                                String mimeTypes = httpProtocol.
-                                        getCompressableMimeTypes();
-                                String mimeTypesWithJson = mimeTypes + "," + MediaType.APPLICATION_JSON_VALUE + ",application/javascript";
-                                httpProtocol.setCompressableMimeTypes(
-                                        mimeTypesWithJson);
-                            }
-                        }
-                        );
-            }
-        };
-    }
-
 }
